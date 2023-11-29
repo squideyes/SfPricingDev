4f0158758b8848ed249207905a49c0cb1e97d758
Louis S. Berman <louis@squideyes.com>
2023-11-29 15:00:23 -0500

    Need to extract the benefits

---
+++ SfPricingDev/Helpers/CouponArgs.cs
@@ -0,0 +1,8 @@
+﻿namespace SfPricingDev;
+
+public class CouponArgs : EventArgs
+{
+    public required string Coupon { get; init; }
+    public required bool IsValid { get; init; }
+    public required double? DiscountPercent { get; init; }
+}
